fa6523a2457881426ef1c17baab87d58d35a904d
Evan <rideburtonsnow3@gmail.com>
2018-04-06 13:46:48 -0600

    new test sendSurvey

---
+++ tests/EW_sendSurvey.js
@@ -0,0 +1,13 @@
+const selectors = require('../sources/selectors')
+const data = require('../sources/data')
+const functions = require('../sources/functions')
+
+module.exports = {
+    beforEach:browser => {
+        browser.url('https://www.surveymonkey.com')
+    },
+    after: browser => {
+        browser
+    },
+
+}
