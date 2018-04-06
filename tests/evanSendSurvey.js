76926110ec5f1ad5bf2c5e3ea30314a043e53855
Evan <rideburtonsnow3@gmail.com>
2018-04-06 15:02:27 -0600

    added sending survey test

---
+++ tests/evanSendSurvey.js
@@ -0,0 +1,16 @@
+const selectors = require('../sources/selectors')
+const data = require('../sources/data')
+const functions = require('../sources/functions')
+
+module.exports = {
+    beforEach:browser => {
+        browser.url('https://www.surveymonkey.com')
+    },
+    after: browser => {
+        browser
+    },
+    'QM-33 Send Survey': browser => {
+        functions.loginFunction(browser, selectors, data.userdata)
+
+    },
+}
