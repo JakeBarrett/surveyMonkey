13074d41d92fc2bc3d4c4bffe9a5263d309cf0a3
Jake Barrett <Jake@Jakes-MacBook.local>
2018-04-09 12:15:37 -0600

    cleaned up

--- tests/EW4_deletingTest.js
+++ tests/EW4_deletingTest.js
@@ -2,16 +2,16 @@
 // const data = require('../sources/data')
 // const functions = require('../sources/functions')
 
-module.exports = {
-    beforEach:browser => {
-        browser.url('https://www.surveymonkey.com')
-    },
-    after: browser => {
-        browser
-    },
+// module.exports = {
+//     beforEach:browser => {
+//         browser.url('https://www.surveymonkey.com')
+//     },
+//     after: browser => {
+//         browser
+//     },
 
-    'QM-7 Deleting Survey': browser =>{
-        functions.loginFunction(browser, selectors, data.userdata)
-        functions.deleteSurvey(browser)
-    },
-}
+//     'QM-7 Deleting Survey': browser =>{
+//         functions.loginFunction(browser, selectors, data.userdata)
+//         functions.deleteSurvey(browser)
+//     },
+// }
